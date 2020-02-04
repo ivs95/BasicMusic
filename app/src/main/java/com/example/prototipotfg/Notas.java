815b63042c1e10217c178873952203197e8e376d
ivs95 <ivs199522@gmail.com>
2020-02-04 18:12:51 +0100

    Mezcla

--- app/src/main/java/com/example/prototipotfg/Notas.java
+++ app/src/main/java/com/example/prototipotfg/Notas.java
@@ -1,49 +1,30 @@
 package com.example.prototipotfg;
 
 public enum Notas {
-    DO1("Do1", 31.7, 33.7, "C.wav", 1),
-    DOS1("Do#1",33.71, 35.65, "C#.wav", 1),
-    RE1("Re1", 35.71, 37.71, "D.wav", 1),
-    RES1("Re#1", 37.89, 39.89, "D#.wav", 1),
-    MI1("Mi1", 40.2, 42.2, "E.wav",1),
-    FA1("Fa1", 42.65, 44.65, "piano/F#1.wav", 1),
-    FAS1("Fa#1", 45.25, 47.25, "piano/F#1.wav",1),
-    SOL1("Sol1", 48, 50, "piano/G1.wav", 1),
-    SOLS1("Sol#1", 50.91, 52.91, "piano/G#1.wav", 1),
-    LA1("La1", 54, 56, "piano/A1.wav",1),
-    LAS1("La#1", 57.27, 59.27, "piano/A#1.wav",1),
-    SI1("Si1", 60.74, 62.74, "piano/B1.wav",1),
-    DO2("Do2", "piano/C1.wav", 2),
-    DOS2("Do#2","piano/C#1.wav", 2),
-    RE2("Re2","piano/D1.wav", 2),
-    RES2("Re#2","piano/D#1.wav", 2),
-    MI2("Mi2","piano/E1.wav.wav",2),
-    FA2("Fa2", "piano/F#1.wav", 2),
-    FAS2("Fa#2", "piano/F1.wav",2),
-    SOL2("Sol2", "piano/G1.wav", 2),
-    SOLS2("Sol#2","piano/G#1.wav", 2),
-    LA2("La2","piano/A1.wav",2),
-    LAS2("La#2","piano/A#1.wav",2),
-    SI2("Si2","piano/B1.wav",2);
-
+    DO1("Do", 31.7, 33.7, "C.wav"),
+    DOS1("Do#",33.71, 35.65, "C#.wav"),
+    RE1("Re", 35.71, 37.71, "D.wav"),
+    RES1("Re#", 37.89, 39.89, "D#.wav"),
+    MI1("Mi", 40.2, 42.2, "E.wav"),
+    FA1("Fa", 42.65, 44.65, "piano/F#1.wav" ),
+    FAS1("Fa#", 45.25, 47.25, "piano/F#1.wav"),
+    SOL1("Sol", 48, 50, "piano/G1.wav" ),
+    SOLS1("Sol#", 50.91, 52.91, "piano/G#1.wav" ),
+    LA1("La", 54, 56, "piano/A1.wav"),
+    LAS1("La#", 57.27, 59.27, "piano/A#1.wav"),
+    SI1("Si", 60.74, 62.74, "piano/B1.wav");
 
 
     private String nombre;
     private double minimaFrecuencia;
     private double maximaFrecuencia;
     private String path;
-    private int octava;
-    private Notas(String nombre, double minimaFrecuencia, double maximaFrecuencia, String path,int octava){
+
+    private Notas(String nombre, double minimaFrecuencia, double maximaFrecuencia, String path){
         this.nombre=nombre;
         this.minimaFrecuencia=minimaFrecuencia;
         this.maximaFrecuencia=maximaFrecuencia;
         this.path=path;
-        this.octava=octava;
-    }
-    private Notas(String nombre, String path,int octava){
-        this.nombre=nombre;
-        this.path=path;
-        this.octava=octava;
     }
 
     public String getNombre() {
