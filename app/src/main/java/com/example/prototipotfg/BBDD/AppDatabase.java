baa442c5a107200cfe94ac2bd7470dcf7fbd8837
ivs95 <ivs199522@gmail.com>
2020-03-22 15:39:45 +0100

    AppDatabase ahora tiene getInstance para no construirse siempre.

--- app/src/main/java/com/example/prototipotfg/BBDD/AppDatabase.java
+++ app/src/main/java/com/example/prototipotfg/BBDD/AppDatabase.java
@@ -1,10 +1,25 @@
 package com.example.prototipotfg.BBDD;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(entities = {Usuario.class, NivelAdivinar.class, NivelImitar.class}, version=1)
 public abstract class AppDatabase extends RoomDatabase {
+    private static volatile AppDatabase instance;
     public abstract DAONivel daoNivel();
     public abstract DAOUsuario daoUsuario();
+
+    public static synchronized AppDatabase getInstance(Context context){
+        if (instance == null){
+            instance = create(context);
+        }
+        return instance;
+    }
+
+    private static AppDatabase create(final Context context){
+            return Room.databaseBuilder(context, AppDatabase.class, "database-name").allowMainThreadQueries().build();
+    }
 }
