65cea6879282e45beda975139431c4860828913a
Ernie97 <ernielmd@gmail.com>
2020-04-08 00:54:07 +0200

    rangos

--- app/src/main/java/com/example/prototipotfg/Enumerados/RangosPuntuaciones.java
+++ app/src/main/java/com/example/prototipotfg/Enumerados/RangosPuntuaciones.java
@@ -1,22 +1,20 @@
 package com.example.prototipotfg.Enumerados;
 
 public enum RangosPuntuaciones {
-    Principiante("Principiante", 0, 30),
-    Aprendiz("Aprendiz", 30, 60),
-    Veterano("Veterano", 60, 90),
-    Experto("Experto", 90, 120),
-    Maestro("Maestro", 120, 150),
-    GranMaestro("Gran Maestro", 150, 180),
-    Leyenda("Leyenda", 180, 210);
+    Principiante("Principiante", "drawable/semifusa"),
+    Aprendiz("Aprendiz", "drawable/fusa"),
+    Veterano("Veterano", "drawable/semicorchea"),
+    Experto("Experto", "drawable/corchea"),
+    Maestro("Maestro", "drawable/negra"),
+    GranMaestro("Gran Maestro", "drawable/blanca"),
+    Leyenda("Leyenda", "drawable/redonda");
 
     private String nombre;
-    private int puntuacion_inicio;
-    private int getPuntuacion_fin;
+    private String image;
 
-    RangosPuntuaciones(String nombre, int puntuacion_inicio ,int getPuntuacion_fin){
+    RangosPuntuaciones(String nombre, String image){
         this.nombre=nombre;
-        this.puntuacion_inicio = puntuacion_inicio;
-        this.getPuntuacion_fin = getPuntuacion_fin;
+        this.image = image;
     }
 
     public static RangosPuntuaciones actualizaRango(String modoJuego, int puntuacion){
@@ -28,7 +26,7 @@ public enum RangosPuntuaciones {
                 else if(puntuacion >= 138 && puntuacion < 225) nuevo_rango = RangosPuntuaciones.Veterano;
                 else if(puntuacion >= 225 && puntuacion < 273) nuevo_rango = RangosPuntuaciones.Experto;
                 else if(puntuacion >= 273 && puntuacion < 324) nuevo_rango = RangosPuntuaciones.Maestro;
-                else if(puntuacion >= 324 && puntuacion < 378) nuevo_rango = RangosPuntuaciones.GranMaestro;
+                else if(puntuacion >= 324 && puntuacion < 398) nuevo_rango = RangosPuntuaciones.GranMaestro;
                 else return RangosPuntuaciones.Leyenda;
                 break;
             case "Adivinar_Intervalo":
@@ -36,11 +34,12 @@ public enum RangosPuntuaciones {
             case "Adivinar_Acordes":
 
             case "Crear_Acordes":
-                if(puntuacion >= 0 && puntuacion < 30) nuevo_rango = RangosPuntuaciones.Principiante;
-                else if(puntuacion >= 30 && puntuacion < 63) nuevo_rango = RangosPuntuaciones.Aprendiz;
-                else if(puntuacion >= 63 && puntuacion < 99) nuevo_rango = RangosPuntuaciones.Veterano;
-                else if(puntuacion >= 99 && puntuacion < 138) nuevo_rango = RangosPuntuaciones.Experto;
-                else if(puntuacion >= 138 && puntuacion < 180) nuevo_rango = RangosPuntuaciones.Maestro;
+                if(puntuacion >= 0 && puntuacion < 15) nuevo_rango = RangosPuntuaciones.Principiante;
+                else if(puntuacion >= 15 && puntuacion < 30) nuevo_rango = RangosPuntuaciones.Aprendiz;
+                else if(puntuacion >= 30 && puntuacion < 63) nuevo_rango = RangosPuntuaciones.Veterano;
+                else if(puntuacion >= 63 && puntuacion < 99) nuevo_rango = RangosPuntuaciones.Experto;
+                else if(puntuacion >= 99 && puntuacion < 138) nuevo_rango = RangosPuntuaciones.Maestro;
+                else if(puntuacion >= 138 && puntuacion < 200) nuevo_rango = RangosPuntuaciones.GranMaestro;
                 else nuevo_rango = RangosPuntuaciones.Leyenda;
                 break;
             case "Crear_Intervalo":
@@ -48,11 +47,11 @@ public enum RangosPuntuaciones {
 
             case "Realiza_Ritmo":
                 if(puntuacion >= 0 && puntuacion < 30) nuevo_rango = RangosPuntuaciones.Principiante;
-                else if(puntuacion >= 30 && puntuacion < 138) nuevo_rango = RangosPuntuaciones.Aprendiz;
-                else if(puntuacion >= 138 && puntuacion < 225) nuevo_rango = RangosPuntuaciones.Veterano;
-                else if(puntuacion >= 225 && puntuacion < 273) nuevo_rango = RangosPuntuaciones.Experto;
-                else if(puntuacion >= 273 && puntuacion < 324) nuevo_rango = RangosPuntuaciones.Maestro;
-                else if(puntuacion >= 324 && puntuacion < 378) nuevo_rango = RangosPuntuaciones.GranMaestro;
+                else if(puntuacion >= 30 && puntuacion < 99) nuevo_rango = RangosPuntuaciones.Aprendiz;
+                else if(puntuacion >= 99 && puntuacion < 138) nuevo_rango = RangosPuntuaciones.Veterano;
+                else if(puntuacion >= 138 && puntuacion < 180) nuevo_rango = RangosPuntuaciones.Experto;
+                else if(puntuacion >= 180 && puntuacion < 225) nuevo_rango = RangosPuntuaciones.Maestro;
+                else if(puntuacion >= 225 && puntuacion < 293) nuevo_rango = RangosPuntuaciones.GranMaestro;
                 else nuevo_rango = RangosPuntuaciones.Leyenda;
                 break;
             default:break;
