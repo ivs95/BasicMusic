5d0f1b648cbff6e3d2de10f99f59b28bbe3b38e9
JesusVer <jesusver@ucm.es>
2020-04-13 15:00:29 +0200

    tutorial imitar audio

--- app/src/main/java/com/example/prototipotfg/ImitarAudio/NivelesImitar.java
+++ app/src/main/java/com/example/prototipotfg/ImitarAudio/NivelesImitar.java
@@ -3,9 +3,12 @@ package com.example.prototipotfg.ImitarAudio;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.example.prototipotfg.Enumerados.Dificultad;
@@ -20,6 +23,9 @@ import java.util.HashMap;
 
 public class NivelesImitar extends Activity {
 
+    private PopupWindow popupWindow;
+    private View popupView;
+    private int tutorial = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -56,6 +62,7 @@ public class NivelesImitar extends Activity {
             //Añadimos el botón a la botonera
             llBotonera.addView(button);
         }
+        mostrarPopupTutorial(findViewById(android.R.id.content).getRootView());
     }
 
     public void nivel_seleccionado(View view) throws IOException {
@@ -79,5 +86,85 @@ public class NivelesImitar extends Activity {
         startActivity(i);
     }
 
+    public void mostrarPopupTutorial(View view){
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+
+        popupView = inflater.inflate(R.layout.popup_tutorial_imitaraudio, null);
+
+        // create the popup window
+        //final PopupWindow popupWindow = new PopupWindow(popupView, width, height, true);
+        popupWindow = new PopupWindow(popupView, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, true);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+
+
+        findViewById(R.id.id_niveles).post(new Runnable() {
+            public void run() {
+                popupWindow.showAtLocation(findViewById(R.id.id_niveles), Gravity.CENTER, 0, 0);
+            }
+        });
+
+        // popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+
+    }
+
+    public void next(View view){
+        tutorial++;
+        actualizaPopUp(popupView);
+    }
+
+    public void prev(View view){
+        tutorial--;
+        actualizaPopUp(popupView);
+    }
+
+
+
+    public void actualizaPopUp(View view){
+        Button button = view.findViewById(R.id.popup_imitaraudio_next);
+        if(tutorial == 1){
+            view.findViewById(R.id.popup_imitaraudio_mensaje2).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.popup_imitaraudio_textoFrecuencia).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.popup_imitaraudio_prev).setVisibility(View.GONE);
+
+            view.findViewById(R.id.popup_imitaraudio_mensaje1).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.popup_imitaraudio_mensaje12).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.popup_imitaraudio_mensaje13).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.popup_imitaraudio_botonGrabar).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.popup_imitaraudio_button2).setVisibility(View.VISIBLE);
+        }
+        else if(tutorial == 2){
+
+            view.findViewById(R.id.popup_imitaraudio_mensaje1).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.popup_imitaraudio_mensaje12).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.popup_imitaraudio_mensaje13).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.popup_imitaraudio_mensaje3).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.popup_imitaraudio_button4).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.popup_imitaraudio_botonGrabar).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.popup_imitaraudio_button2).setVisibility(View.INVISIBLE);
+
+            view.findViewById(R.id.popup_imitaraudio_mensaje2).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.popup_imitaraudio_textoFrecuencia).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.popup_imitaraudio_prev).setVisibility(View.VISIBLE);
+
+            button.setText("Siguiente");
+        }
+        else if(tutorial == 3){
+            view.findViewById(R.id.popup_imitaraudio_mensaje2).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.popup_imitaraudio_textoFrecuencia).setVisibility(View.INVISIBLE);
+
+            view.findViewById(R.id.popup_imitaraudio_mensaje3).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.popup_imitaraudio_button4).setVisibility(View.VISIBLE);
+
+            button.setText("Cerrar");
+        }
+        else if(tutorial == 4){
+            popupWindow.dismiss();
+        }
+    }
 
 }
